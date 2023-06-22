8272c6ffee935c1f075e4f028cc7a538b9ed3a1c
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-06-22 11:03:08 -0500

    Initial Commit

---
+++ PhotosPicker2023Demo/PhotosPicker2023DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosPicker2023DemoApp.swift
+//  PhotosPicker2023Demo
+//
+//  Created by Mark Volkmann on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotosPicker2023DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
